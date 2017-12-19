98ed1ab00f80cb624c8a267c28b9104fa9c0baf4
Ryan Roth <rroth@bkprecision.com>
2017-12-18 17:37:30 -0800

    Added more examples (LabView 2017)

--- bk8500functions.py
+++ bk8500functions.py
@@ -1,26 +1,9 @@
-#   bk8500functions.py
-#
-#   Copyright {2017} {B&K Precision Corporation}
-
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-
-#       http://www.apache.org/licenses/LICENSE-2.0
-
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
-#
 
 def cmd8500(cmd , ser):
     print("Command: ", hex(cmd[2]))
     printbuff(cmd)
     ser.write(cmd)
     resp = ser.read(26)
-    errorCheck(resp)
     #print("Resp: ")
     printbuff(resp);
     
@@ -38,18 +21,3 @@ def csum(thing):
     for i in range(len(thing)):
         sum+=thing[i]
     return 0xFF&sum;
-
-def errorCheck(b):
-    x = ""
-    if b[3] == 0x90:
-        print("Checksum Error: ", end='')
-    elif b[3] == 0xA0:
-        print("Parameter Incorrect: ",end='')
-    elif b[3] == 0xB0:
-        print("Unrecognized Command: ",end='')
-    elif b[3] == 0xC0:
-        print("Invalid Command: ",end='')
-    elif b[3] == 0x80:
-        print("Success: ",end='')
-    else:
-        print("Data return: ",end='')
